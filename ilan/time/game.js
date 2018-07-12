5b9cdcfcbaca3398af4018b084258a5a34588113
Ilan Dembinsky <ilandembinsky8@gmail.com>
2018-07-12 09:44:20 +0300

    next step canvas

--- ilan/time/game.js
+++ ilan/time/game.js
@@ -6,6 +6,12 @@ var dy = 4;
 var WIDTH;
 var HEIGHT;
 
+rightDown = false;
+leftDown = false;
+
+
+
+
 function rect(x,y,w,h) {
 	  ctx.beginPath();
 	  ctx.rect(x,y,w,h);
@@ -24,9 +30,33 @@ function circle(x,y,radius){
 	  ctx.fill();
 }
 
+//set rightDown or leftDown if the right or left keys are down
+function onKeyDown(evt) {
+  if (evt.keyCode == 39) rightDown = true;
+  else if (evt.keyCode == 37) leftDown = true;
+}
+
+//and unset them when the right or left key is released
+function onKeyUp(evt) {
+  if (evt.keyCode == 39) rightDown = false;
+  else if (evt.keyCode == 37) leftDown = false;
+}
+
+function onMouseMove(evt) {
+  if (evt.pageX > canvasMinX && evt.pageX < canvasMaxX) {
+    paddlex = evt.pageX - canvasMinX;
+  }
+}
+
+$(document).keydown(onKeyDown);
+$(document).keyup(onKeyUp);
+$(document).mousemove(onMouseMove);
+
 function draw(){
 	  clear();
 	  circle(x,y,10);
+	  if (rightDown) paddlex += 5;
+	  else if (leftDown) paddlex -= 5;
 	  rect(paddlex, HEIGHT-paddleh, paddlew, paddleh);
 	  
 	  if (x + dx > WIDTH || x + dx < 0)
@@ -44,6 +74,11 @@ function draw(){
 	  
 	  x += dx;
 	  y += dy;
+}
+
+function init_mouse() {
+  canvasMinX = $("#canvas").offset().left;
+  canvasMaxX = canvasMinX + WIDTH;
 }
 
 function init_paddle() {
