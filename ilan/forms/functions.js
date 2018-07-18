1e9254c2ca0887fe13c5305eebad9b3b603fb24a
Ilan Dembinsky <ilandembinsky8@gmail.com>
2018-07-18 10:35:22 +0300

    forms 2

--- ilan/forms/functions.js
+++ ilan/forms/functions.js
@@ -1,15 +1,21 @@
 function startProgram(){
-	x = Math.floor((Math.random() * 100) + 1);
-	y = Math.floor((Math.random() * 100) + 1);
-	document.getElementById("par1").innerHTML = x;
-	document.getElementById("par2").innerHTML = y;
+	for (i=1;i<=5;i++){
+		x = Math.floor((Math.random() * 100) + 1);
+		y = Math.floor((Math.random() * 100) + 1);
+		document.getElementById("par"+i+"1").innerHTML = x;
+		document.getElementById("par"+i+"2").innerHTML = y;
+	}
 }
 
 function check(){
-	par3 = parseInt(document.getElementById("par3").value);
-	par1 = parseInt(document.getElementById("par1").innerHTML);
-	par2 = parseInt(document.getElementById("par2").innerHTML);
-	if (par1+par2==par3)
+	result = 0;
+	for (i=1;i<=5;i++){
+		par3 = parseInt(document.getElementById("par"+i+"3").value);
+		par1 = parseInt(document.getElementById("par"+i+"1").innerHTML);
+		par2 = parseInt(document.getElementById("par"+i+"2").innerHTML);
+		if (par1+par2==par3) result = result + 20;
+	}
+	if (result>=80)
 		alert("Sachtein");
 	else
 		alert("Not Sachtein");
